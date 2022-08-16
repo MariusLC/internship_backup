f554caa659c17bcf235d144af9b9f5496c59e9b7
MariusLC <marius.le_chapelier@etu.sorbonne-universite.fr>
2022-08-16 15:02:55 +0200

    new moral 111

--- moral_rl/envs/gym_wrapper.py
+++ moral_rl/envs/gym_wrapper.py
@@ -104,8 +104,7 @@ class GymWrapper(gym.Env):
 
     def step(self, action):
         obs, reward, _ = self.game.play(action)
-        print(obs.board)
-        print(obs._board)
+        # print(obs.board)
         return self._obs_to_np_array(obs), reward, self.game.game_over, self.game.the_plot
 
     def step_demo(self, action):
